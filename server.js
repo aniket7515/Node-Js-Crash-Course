42be2bd1e2c6c9cf0d394877bbbbb09f376c18ed
Aniket <aniketdhokane2071@gmail.com>
2021-12-04 15:09:03 +0530

    Returning HTML pages

--- server.js
+++ server.js
@@ -1,13 +1,42 @@
 // Creating a Server
 
 const http= require('http');
+const fs=require('fs');
 
 const server = http.createServer((req,res)=>{
-    console.log('request made');
+    console.log(req.url , req.method);
+
+    // Set header content type and content
+    res.setHeader('Content-Type', 'text/html');
+
+    // Send an HTML file 
+    fs.readFile('./views/index.html',(err,data)=>{
+        if(err){
+            console.log(err);
+            res.end();
+        }else{
+            // res.write(data);
+            // We can also use res.end(data) directly to send file
+            res.end(data);
+        }
+    })
+
+
+   
 
 });  // Method to create server 
 
 
-server.listen(3000, 'localhost',()=>{
+server.listen(3000,()=>{
     console.log('Listening on port 3000');
 });
+
+
+// LOCAL HOST
+
+// 1) localhost is like a domain name on the web 
+// 2) local host ------------------> 127.0.0.1---------------------> Own Computer 
+
+// PORT NUMBERS
+
+// 1)Port numbers are like doors into computers 
